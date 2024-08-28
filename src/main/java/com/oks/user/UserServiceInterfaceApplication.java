44074e5c3868e3f69410e0ad5fb5f4fce7fa8f12
Omkar Singh <omkarsinghaws@gmail.com>
2024-08-28 21:17:32 +0530

    First commit

---
+++ src/main/java/com/oks/user/UserServiceInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.oks.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oks/user/UserServiceInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oks.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
